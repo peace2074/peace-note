6ecfde23bab3649a52c93468581bd650a67dc150
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2022-09-01 10:16:28 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ import PeaceToast from "./PeaceToast.js"
 document.querySelector("button").addEventListener("click", () => {
   const pt = new PeaceToast({
     text: "Hello",
-    position: "top-left",
+    position: "top-right",
     pauseOnHover: true,
     pauseOnFocusLoss: true,
   })
